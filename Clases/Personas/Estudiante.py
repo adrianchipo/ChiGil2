1f0b9d7869f271467cfcddcea3ecf69ed7e2eecf
Adrián Chi <adrianchi@Adrians-MacBook-Pro.local>
2019-12-03 12:31:33 -0500

    hotfix

--- Clases/Personas/Estudiante.py
+++ Clases/Personas/Estudiante.py
@@ -2,7 +2,7 @@ from PersonaBase import PersonaClaseBase
 
 class Estudiante(PersonaClaseBase):
     def __init__(self, Usuario=None, LimiteLibros=3, LimiteRenovacion=3):
-        PersonaClaseBase.__init__(self, LimiteLibros, LimiteRenovacion)
+        PersonaClaseBase.__init__(self, Usuario, LimiteLibros, LimiteRenovacion)
     
     def BuscarLibro(self):
         print('buscar libro')
