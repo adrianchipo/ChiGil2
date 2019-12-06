2421a1083571e8be3c62639fa2c508bfffceb19d
Adrián Chi <adrianchi@Adrians-MacBook-Pro.local>
2019-12-06 17:12:00 -0500

    hotfix

--- Clases/Libro.py
+++ Clases/Libro.py
@@ -2,18 +2,19 @@ import time
 import datetime
 
 class Libro:
-    def __init__ (self, Titulo, Autor, FechaPrestamo, FechaEntrega, Renovado, Atrasado):
+    def __init__ (self, Titulo, Autor, FechaPrestamo, FechaEntrega, Renovado, Atrasado, Prestado):
         self.Titulo = Titulo
         self.Autor = Autor
         self.FechaPrestamo = FechaPrestamo
         self.FechaEntrega = FechaEntrega
         self.Renovado = Renovado
         self.Atrasado = Atrasado
+        self.Prestado = Prestado
 
     def CalcularFechaEntrega(self):
         self.FechaEntrega = self.FechaPrestamo + datetime.timedelta(days=7)
 
-libro1 = Libro("abc", "juan", datetime.date(2018,1,1), datetime.date(2018,1,1), False, False)
+libro1 = Libro("abc", "juan", datetime.date(2018,1,1), datetime.date(2018,1,1), False, False, False)
 libro1.CalcularFechaEntrega()
 print(libro1.FechaEntrega)
 
